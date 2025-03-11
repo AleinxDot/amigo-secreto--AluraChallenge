b07ab400f89ede9aec45bab2b57f148e47d5e0c6
AleinxDot <agonzalesp@unasam.edu.pe>
2025-03-11 18:23:52 -0500

    Actualizando app.js

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+
+    let amigo = document.getElementById("amigo").value;
+    console.log(amigo);
+
+    if (amigo == null || amigo == "") {
+        alert("Debe ingresar un nombre de amigo");
+        return;
+    }
+    if (listaDeAmigos.includes(amigo)) {
+        alert("El amigo ya se encuentra en la lista");
+        return;
+    }
+    else{
+        listaDeAmigos.push(amigo);
+        document.getElementById("amigo").value = "";
+        mostrarAmigos();
+    }
+
+}
+function mostrarAmigos(){
+
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        lista.innerHTML += "<li>" + listaDeAmigos[i] + "</li>";
+    }
+
+}
+function sortearAmigo(){
+
+    let numeroDeAmigos = listaDeAmigos.length;
+    if (numeroDeAmigos >= 0) {
+        alert("No hay amigos en la lista");
+        return;
+    }
+    if (numeroDeAmigos == 1) {
+        alert("No se puede sortear con un solo amigo");
+        return;
+    }
+    else{
+        let indiceSorteado = Math.floor(Math.random() * numeroDeAmigos);
+        let amigoSorteado = listaDeAmigos[indiceSorteado];
+        document.getElementById('resultado').innerHTML = '<li>'+amigoSorteado+'</li>';
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Bienvenido!!💻👩‍💻
+Bienvenido a mi repositorio del <b>Challenge de Amigo Secreto</b> de alura latam.
+
+## Acceso:
+Podras Acceder al proyecto de dos formas:
+
+1. Enlace de Github Pages
+
+Para acceder mediante Github Pages, deberas seguir el siguiente enlace:
+
+
+
+2. Enlace de Vercel
+Para acceder mediante Github Pages, deberas seguir el siguiente enlace:
